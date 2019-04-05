20e706322af85bdf379e7cc5ce994fa607643e54
Dana Weiss <mynameisdanaweiss@gmail.com>
2019-04-05 15:40:46 -0700

    update Game&Phrase with required boilerplate functions/props

--- classes/Game.php
+++ classes/Game.php
@@ -1,5 +1,23 @@
 <?php
 
 class Game {
-  
+  private $phrase;
+  private $lives;
+
+  // private function checkForWin() {
+  //
+  // }
+  //
+  // private function gameOver() {
+  //
+  // }
+  //
+  // private function displayKeyboard() {
+  //
+  // }
+  //
+  // private function displayScore() {
+  //
+  // }
+
 }
--- classes/Phrase.php
+++ classes/Phrase.php
@@ -3,4 +3,13 @@
 class Phrase {
   private $currentPhrase = " ";
   private $selected = [ ];
+
+  // private function addPhraseToDisplay() {
+  //
+  // }
+  //
+  // private function checkLetter() {
+  //
+  // }
+
 }
