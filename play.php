dd9f73fd850595a4e57b37430b1065df00e1645d
Dana Weiss <mynameisdanaweiss@gmail.com>
2019-04-10 22:26:56 -0700

    whitespace cleanup

--- play.php
+++ play.php
@@ -8,36 +8,35 @@ $score = new Game;
 
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<meta charset="utf-8">
-		<title>Phrase Hunter</title>
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<link href="css/styles.css" rel="stylesheet">
-		<link href="css/animate.css" rel="stylesheet">
-		<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
-	</head>
-	<body>
+  <head>
+    <meta charset="utf-8">
+    <title>Phrase Hunter</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/styles.css" rel="stylesheet">
+    <link href="css/animate.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+  </head>
+  <body>
 
-		<div class="main-container">
-      <div id='scoreboard' class='section'>
-        <ol>
-          <?php $score->displayScore(); ?>
-        </ol>
-      </div>
+  <div class="main-container">
+    <div id='scoreboard' class='section'>
+      <ol>
+        <?php $score->displayScore(); ?>
+      </ol>
+  </div>
 
-			<h2 class="header">Phrase Hunter</h2>
-      <div id='phrase' class='section'>
-        <ul>
-          <?php $game->addPhraseToDisplay(); ?>
-        </ul>
-      </div>
+  <h2 class="header">Phrase Hunter</h2>
+  <div id='phrase' class='section'>
+    <ul>
+      <?php $game->addPhraseToDisplay(); ?>
+    </ul>
+  </div>
 
-      <form action="play.php">
-        <div id='qwerty' class='section'>
-          <?php $score->displayKeyboard(); ?>
-        </div>
-      </form>
-		</div>
+  <form action="play.php">
+    <div id='qwerty' class='section'>
+      <?php $score->displayKeyboard(); ?>
+    </div>
+  </form>
 
-	</body>
+  </body>
 </html>
