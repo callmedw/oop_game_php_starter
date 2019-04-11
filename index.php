70ade13cb2d6eac565da0d7e53309c048323b22e
Dana Weiss <mynameisdanaweiss@gmail.com>
2019-04-10 22:33:02 -0700

    html header and footer files inc

--- index.php
+++ index.php
@@ -1,22 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-	<head>
-		<meta charset="utf-8">
-		<title>Phrase Hunter</title>
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<link href="css/styles.css" rel="stylesheet">
-		<link href="css/animate.css" rel="stylesheet">
-		<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
-	</head>
-	<body>
+<?php include "inc/header.php"; ?>
 
-		<div class="main-container">
-			<h2 class="header">Phrase Hunter</h2>
+	<h2 class="header">Phrase Hunter</h2>
 
-			<form action="play.php">
-				<input id="btn__reset" type="submit" value="Start Game" />
-			</form>
-		</div>
+	<form action="play.php">
+		<input id="btn__reset" type="submit" value="Start Game" />
+	</form>
 
-	</body>
-</html>
+<?php include "inc/footer.php"; ?>
--- play.php
+++ play.php
@@ -1,44 +1,29 @@
 <?php
 include "classes/Phrase.php";
 include "classes/Game.php";
+include "inc/header.php";
 
 $game = new Phrase("yoshi is the supreme being");
 $score = new Game;
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <title>Phrase Hunter</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="css/styles.css" rel="stylesheet">
-    <link href="css/animate.css" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
-  </head>
-  <body>
-    <div class="main-container">
+  <div id='scoreboard' class='section'>
+    <ol>
+      <?php $score->displayScore(); ?>
+    </ol>
+  </div>
 
-      <div id='scoreboard' class='section'>
-        <ol>
-          <?php $score->displayScore(); ?>
-        </ol>
-      </div>
+  <h2 class="header">Phrase Hunter</h2>
+  <div id='phrase' class='section'>
+    <ul>
+      <?php $game->addPhraseToDisplay(); ?>
+    </ul>
+  </div>
 
-
-      <h2 class="header">Phrase Hunter</h2>
-      <div id='phrase' class='section'>
-        <ul>
-          <?php $game->addPhraseToDisplay(); ?>
-        </ul>
-      </div>
-
-      <form action="play.php">
-        <div id='qwerty' class='section'>
-          <?php $score->displayKeyboard(); ?>
-        </div>
-      </form>
-
+  <form action="play.php">
+    <div id='qwerty' class='section'>
+      <?php $score->displayKeyboard(); ?>
     </div>
-  </body>
-</html>
+  </form>
+
+<?php include "inc/footer.php"; ?>
